1f10dbaa2c655eb68fba26660d6f81ba06eb0567
Areocodr <jibo.areo@gmail.com>
2026-09-11 14:56:08 +0100

    Dashboard (stats section) update complete

--- src/features/dashboard/Dashboard.jsx
+++ src/features/dashboard/Dashboard.jsx
@@ -1,5 +1,23 @@
+import mockStudents from "../../data/mockStudents.js";
+import StatCard from "./StatCard.jsx"
+import styles from './Dashboard.module.css';
+
 function Dashboard() {
-    return <h2>Dashboard</h2>
+    const totalStudents = mockStudents.length;
+    const activeCount = mockStudents.filter((s) => s.status === "active").length;
+    const blacklistedCount = mockStudents.filter((s) => s.status === "blacklisted").length;
+    
+    return (
+        <div className={styles.dashboard}>
+            <h2 className={styles.heading}>Dashboard</h2>
+
+            <div className={styles.statsGrid}>
+                <StatCard label="Total Students" value={totalStudents} />
+                <StatCard label="Active Students" value={activeCount} />
+                <StatCard label="Blacklisted Students" value={blacklistedCount} />
+            </div>
+        </div>
+    );  
 }
 
 export default Dashboard;
